4c03d7736a1c67d32f22a143e949c4b8d9feb9f6
fm ti <starprefix@gmail.com>
2021-12-02 15:09:13 +0330

    first

---
+++ src/test/java/sampleTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class sampleTest {
+	@Test
+	public void simpleTestPass() {
+		int a = 1;
+		int b = 2;
+		assertTrue(a + b == 3);
+	}
+	/*@Test
+	public void simpleTestFail() {
+		int a = 1;
+		int b = 2;
+		assertTrue(a + b == 120);
+	}*/
+	@Test
+	public void simpleTestPass2() {
+		int a = 100;
+		int b = 20;
+		assertTrue(a + b == 120);
+	}
+
+}
